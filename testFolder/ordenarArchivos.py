c372ff1cb6afdee969ec2516e337f1ef594c38f6
David Fernández Quiñones <dfq240@inlumine.ual.es>
2024-02-12 21:15:18 +0100

    Meter en una carpeta todos los archivos que tengas

---
+++ testFolder/ordenarArchivos.py
@@ -0,0 +1,11 @@
+import os
+
+nueva_carpeta = "files"
+
+if not os.path.exists(nueva_carpeta):
+    os.makedirs(nueva_carpeta)
+
+with os.scandir(os.getcwd()) as ls:
+    for entrada in ls:
+        if entrada.is_file():
+            os.rename(entrada.path, os.path.join(nueva_carpeta, entrada.name))
