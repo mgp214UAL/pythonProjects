6316bdeee6397475c6b37f93dd7c6fb70aad366c
David Fernández Quiñones <dfq240@inlumine.ual.es>
2024-02-12 21:45:24 +0100

    prueba

--- testFolder/ordenarArchivos.py
+++ testFolder/ordenarArchivos.py
@@ -6,6 +6,6 @@ if not os.path.exists(nueva_carpeta):
     os.makedirs(nueva_carpeta)
 
 with os.scandir(os.getcwd()) as ls:
-    for entrada in ls:
+    for entrada in ls: 
         if entrada.is_file():
             os.rename(entrada.path, os.path.join(nueva_carpeta, entrada.name))
